cae498600cf64d409250824a34892286e5697e4d
weaall <weaall88@gmail.com>
2024-01-14 00:01:55 +0900

    Updates

--- _next/static/pPkKibXtKHuA28sOkT8mh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-228a1c6c96ec611b.js"],"/shallow/[slug]":["static/chunks/pages/shallow/[slug]-fea702f939230b91.js"],sortedPages:["/_app","/_error","/shallow/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
