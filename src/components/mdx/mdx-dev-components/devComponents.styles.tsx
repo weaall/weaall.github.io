ca87757a42cd7481e48c4dacd304c47e55a94532
weaall <weaall@naver.com>
2024-09-02 23:22:43 +0900

    fix : hotel api

--- src/components/mdx/mdx-dev-components/devComponents.styles.tsx
+++ src/components/mdx/mdx-dev-components/devComponents.styles.tsx
@@ -1,8 +1,8 @@
 import tw from "tailwind-styled-components";
 
 export const H1 = tw.h1`text-3xl font-bold`
-export const H2 = tw.h2`text-2xl font-bold mt-0 mb-9 after:content-['.'] after:text-red-500 cursor-pointer`
-export const H3 = tw.h3`text-xl font-bold mt-9 mb-6`
+export const H2 = tw.h2`text-[1.8rem] font-bold mt-0 mb-9 after:content-['.'] after:text-red-500 cursor-pointer`
+export const H3 = tw.h3`text-[1.4rem] font-bold mt-9 mb-6`
 
 export const CheckBoxWrap = tw.div`flex space-x-6 items-center my-3`
 export const CheckBoxLabel = tw.p`text-lg`
@@ -12,18 +12,18 @@ export const P = tw.p`text-lg py-3`
 
 export const A = tw.a`text-lg font-bold text-red-500`
 
-export const Hr= tw.hr`my-20 w-full h-[2px] bg-t-main`
+export const Hr= tw.hr`my-14 w-full h-[2px] bg-t-main`
 
 export const Pre = tw.pre`text-t-main text-base`
 
 export const Li = tw.li`pl-4 py-1 text-base before:content-['-'] before:font-bold before:pr-3`
 
-export const CodeWrap = tw.div`relative bg-main rounded-xl text-t-main text-base p-6 mb-12 mt-4`
+export const CodeWrap = tw.div`relative bg-main rounded-xl text-t-main text-sm p-6 mb-12 mt-4`
 export const CodeWrapC = tw.div`relative rounded-xl text-main text-base pt-8 mb-10`
 export const ClassWrap = tw.div`absolute top-0 rounded-2xl py-3 px-6 bg-white w-fit ml-8` 
 export const ClassLabel = tw.p`text-2xl font-bold after:content-['.'] after:text-red-500`
 export const CodeBoxC = tw.div`bg-main p-6 pt-12 rounded-xl`
-export const Code = tw.code` rounded-xl text-t-main my-6 text-base`
+export const Code = tw.code` rounded-xl text-t-main my-6 text-sm`
 
 export const Strong = tw.strong`font-bold`
 
