73874abbf4ff4d1eed7eb37db028fafb0441ea00
weaall <weaall@naver.com>
2024-11-12 10:29:10 +0900

    Updates

--- _next/static/chunks/app/(pages)/prac/[slug]/page-38587e411dce1b4f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[381],{9918:function(n,e,u){Promise.resolve().then(u.t.bind(u,2927,23))},2927:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=9918)}),_N_E=n.O()}]);
