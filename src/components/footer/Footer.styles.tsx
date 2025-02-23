fbc6fc6de79f68c8c49ff97e62b5972bfd252d2d
weaall <weaall@naver.com>
2025-02-24 00:13:55 +0900

    fix : footer styles

--- src/components/footer/Footer.styles.tsx
+++ src/components/footer/Footer.styles.tsx
@@ -1,17 +1,19 @@
 import tw from "tailwind-styled-components";
 
-export const Container = tw.div`flex flex-col w-full h-full border-t mt-20 text-main pt-16 px-28 select-none
-`
+export const Container = tw.div`flex flex-col w-full h-full mt-10 text-main pt-4 px-10 select-none
+mobile:px-12`
 
-export const FooterWrap = tw.div`flex max-w-[1080px] justify-between flex mobile:flex-col
-pb-10`
+export const FooterWrap = tw.div`flex max-w-[1080px] h-full pb-10 border-t pt-16
+mobile:flex-col-reverse`
 
-export const LeftWrap = tw.div`flex flex-col text-left items-start w-[50%]`
-export const MainLabel = tw.p`font-semibold text-main text-lg py-2`
-export const MainP = tw.p`text-xs py-0.5`
-export const LogoSvg = tw.img`w-10 w-10`
+export const LeftWrap = tw.div`flex flex-col text-left items-start w-[50%] 
+mobile:w-full mobile:justify-center mobile:mx-auto mobile:text-center`
+export const MainLabel = tw.p`font-semibold text-main text-lg py-2 mobile:mx-auto`
+export const MainP = tw.p`text-xs py-0.5 mobile:mx-auto`
+export const LogoSvg = tw.img`w-10 w-10 mobile:mx-auto`
 
-export const RightWrap = tw.div`flex justify-end space-x-16 w-[50%]`
+export const RightWrap = tw.div`flex justify-end space-x-16 w-[50%] 
+mobile:w-full mobile:justify-center mobile:mx-auto mobile:pb-10`
 export const ColWrap = tw.div`flex flex-col space-y-2.5 text-left`
 export const TopLabel = tw.label`text-xs font-semibold`
 export const Index = tw.a`text-xs`
