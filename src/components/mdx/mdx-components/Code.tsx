3fe70cb5301e8e86ed4036b564794c40f18f06f5
wi <weaall@mindsai.co.kr>
2026-07-13 00:57:18 +0900

    fix: few

--- src/components/mdx/mdx-components/Code.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import CodeBlock from "../CodeBlock";
-import * as tw from "./components.styles";
-
-export default function Code(props: { className?: string; children?: React.ReactNode }) {
-    return <CodeBlock {...props} styles={tw} />;
-}
--- src/components/mdx/mdx-dev-components/Code.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import CodeBlock from "../CodeBlock";
-import * as tw from "./devComponents.styles";
-
-export default function Code(props: { className?: string; children?: React.ReactNode }) {
-    return <CodeBlock {...props} styles={tw} />;
-}
